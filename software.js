18b959340708b4a0939915b4e111f7cc5d8a74cb
Dave Churchill <dave@ninthcuriosity.com>
2012-03-08 18:06:56 +1100

    Updated tape metadata
    - Added a number new tapes from Richard Larkin
    - Added author information where known
    - Fixed tapeParameters for Robot Fire

--- software.js
+++ software.js
@@ -26,20 +26,10 @@ nanowasp.software = [
         filename: "camel.mwb"
     },
     {
-        title: "Depth Charge",
-        url: "software/d-charge.bee",
-        filename: "d-charge.bee"
-    },
-    {
         title: "Frankenstein's Monster",
         url: "software/frank.mwb",
         filename: "frank.mwb",
         tapeParameters: ["B", 0x08C0, 0x0000, false, 0x47]
-    },
-    {
-        title: "Towers of Hanoi",
-        url: "software/hanoi.mwb",
-        filename: "hanoi.mwb"
     },
     {
         title: "Jeksil's Revenge",
@@ -47,18 +37,104 @@ nanowasp.software = [
         filename: "jeksil.mwb"
     },
     {
-        title: "Lazer",
-        url: "software/lazer.bee",
+        title: "The Towers of Hanoi",
+        url: "software/hanoi.mwb",
+        filename: "hanoi.mwb"
+    },
+
+    {
+        title: "Depth Charge",
+        author: "Brad Robinson",
+        url: "software/brad_robinson/d-charge.bee",
+        filename: "d-charge.bee"
+    },
+    {
+        title: "Laser Blazer",
+        author: "Brad Robinson",
+        url: "software/brad_robinson/lazer.bee",
         filename: "lazer.bee"
     },
     {
-        title: "Robotf",
-        url: "software/robotf.bee",
-        filename: "robotf.bee"
+        title: "Robot Fire",
+        author: "Brad Robinson",
+        url: "software/brad_robinson/robotf.bee",
+        filename: "robotf.bee",
+        tapeParameters: ["M", 0x03fc, 0x1983, true, 0x00]
     },
     {
         title: "Space Lanes",
-        url: "software/sp-lanes.bee",
+        author: "Brad Robinson",
+        url: "software/brad_robinson/sp-lanes.bee",
         filename: "sp-lanes.bee"
+    },
+
+    {
+        title: "Bounce",
+        author: "Richard Larkin",
+        url: "software/richard_larkin/bounce.mwb",
+        filename: "bounce.mwb"
+    },
+    {
+        title: "Break Out",
+        author: "Richard Larkin",
+        url: "software/richard_larkin/bricks.mwb",
+        filename: "bricks.mwb"
+    },
+    {
+        title: "Catack",
+        author: "Richard Larkin",
+        url: "software/richard_larkin/catack.bee",
+        filename: "catack.bee"
+    },
+    {
+        title: "Catter",
+        author: "Richard Larkin",
+        url: "software/richard_larkin/catter.mwb",
+        filename: "catter.mwb"
+    },
+    {
+        title: "Catter 2",
+        author: "Richard Larkin",
+        url: "software/richard_larkin/cater2.mwb",
+        filename: "cater2.mwb"
+    },
+    {
+        title: "Catter 3",
+        author: "Richard Larkin",
+        url: "software/richard_larkin/cater3.mwb",
+        filename: "cater3.mwb"
+    },
+        {
+        title: "Earth",
+        author: "Richard Larkin",
+        url: "software/richard_larkin/earth.mwb",
+        filename: "earth.mwb"
+    },
+    {
+        title: "Isbok Adventure.mwb",
+        author: "Richard Larkin",
+        url: "software/richard_larkin/isbok-g.mwb",
+        filename: "isbok-g.mwb"
+    },
+    {
+        title: "Mazes!",
+        author: "Richard Larkin",
+        url: "software/richard_larkin/mazes.bee",
+        filename: "mazes.bee",
+        tapeParameters: ["M", 0x4000, 0x4000, true, 0x00]
+    },
+    {
+        title: "Othello",
+        author: "Richard Larkin",
+        url: "software/richard_larkin/othello.bee",
+        filename: "othello.bee",
+        tapeParameters: ["M", 0x0400, 0x0400, true, 0x00]
+    },
+    {
+        title: "Pucker",
+        author: "Richard Larkin",
+        url: "software/richard_larkin/pucker.bee",
+        filename: "pucker.bee",
+        tapeParameters: ["M", 0x03a2, 0x03a2, true, 0x00]
     }
 ];
